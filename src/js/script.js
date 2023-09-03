bb07234ab5c6699292ba2eb15fbf354d720c1aff
Kostya-x-pro <Kostya-voronkov1992@rambler.ru>
2023-09-04 01:13:27 +0300

    Added a feedback form. Added slick slider. Adapted sections for mobile devices

--- src/js/script.js
+++ src/js/script.js
@@ -1,4 +1,26 @@
 window.addEventListener('DOMContentLoaded', () => {
+  // slick slider
+    $('.carusel__inner').slick({
+      speed: 1000,
+      adaptiveHeight: true,
+      autoplay: true,
+      autoplaySpeed: 4000,
+      prevArrow: '<button type="button" class="slick-prev"><img src="icons/left_arrow.svg"></button>',
+      nextArrow: '<button type="button" class="slick-next"><img src="icons/right_arrow.svg"></button>',
+      responsive: [
+        {
+          breakpoint: 767,
+          settings: {
+            dots: true,
+            arrows: false,
+            slidesToShow: 1,
+          }
+        },
+        
+      ]
+    });
+
+
   //Bureger menu
   const burgerBtn = document.querySelector('#burger');
 

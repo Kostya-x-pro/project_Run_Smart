244803f42e0ae1f3866004686f1ff461f663ddd2
Kostya-x-pro <Kostya-voronkov1992@rambler.ru>
2023-09-04 18:02:59 +0300

    Added a catalog section , implemented tabs on the page. Added a section with reviews. Adapted the added content for mobile devices.

--- src/js/script.js
+++ src/js/script.js
@@ -2,7 +2,6 @@ window.addEventListener('DOMContentLoaded', () => {
   // slick slider
     $('.carusel__inner').slick({
       speed: 1000,
-      adaptiveHeight: true,
       autoplay: true,
       autoplaySpeed: 4000,
       prevArrow: '<button type="button" class="slick-prev"><img src="icons/left_arrow.svg"></button>',
@@ -20,6 +19,25 @@ window.addEventListener('DOMContentLoaded', () => {
       ]
     });
 
+    // tabs
+    $('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function() {
+      $(this)
+        .addClass('catalog__tab_active').siblings().removeClass('catalog__tab_active')
+        .closest('div.container').find('div.catalog__content').removeClass('catalog__content_active').eq($(this).index()).addClass('catalog__content_active');
+    });
+
+    function togleSlide(item) {
+      $(item).each(function(i){
+        $(this).on('click', function(e) {
+          e.preventDefault();
+          $('.catalog-item__content').eq(i).toggleClass('catalog-item__content_active');
+          $('.catalog-item__list').eq(i).toggleClass('catalog-item__list_active');
+        });
+      });
+    }
+
+    togleSlide('.catalog-item__link');
+    togleSlide('.catalog-item__back');
 
   //Bureger menu
   const burgerBtn = document.querySelector('#burger');
